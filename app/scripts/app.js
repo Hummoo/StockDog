8d86f16b6fd1ac92a49cab52b4d19d5ac56849a7
Tagdanai Sujinnapram <hummoosr17@gmail.com>
2017-05-14 16:11:46 +0700

    Finish step 1

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,11 +19,5 @@ angular
   ])
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+      
   });
